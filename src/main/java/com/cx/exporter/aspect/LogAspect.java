60f83a06a9424bc7540af219c5de7cbe8f006e98
xingce <271541760@qq.com>
2024-06-09 14:06:29 +0800

    update log logic

--- src/main/java/com/cx/exporter/aspect/LogAspect.java
+++ src/main/java/com/cx/exporter/aspect/LogAspect.java
@@ -24,23 +24,6 @@ public class LogAspect {
 
     @Around(value = "pointCut()")
     public Object log(ProceedingJoinPoint joinPoint) {
-        // 开始打印请求日志
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
-        // 打印请求相关参数
-        log.info("========================================== Start ==========================================");
-        // 打印请求 url
-        log.info("URL            : {}", request.getRequestURL().toString());
-        // 打印 Http method
-        log.info("HTTP Method    : {}", request.getMethod());
-        // 打印调用 controller 的全路径以及执行方法
-        log.info("Class Method   : {}.{}", joinPoint.getSignature().getDeclaringTypeName(),
-                joinPoint.getSignature().getName());
-        // 打印请求的 IP
-        log.info("IP             : {}", request.getRemoteAddr());
-        // 打印请求入参
-        log.info("Request Args   : {}", JSONUtil.toJsonPrettyStr(joinPoint.getArgs()));
-
         long startTime = System.currentTimeMillis();
         Object result = null;
         try {
@@ -48,10 +31,23 @@ public class LogAspect {
         } catch (Throwable e) {
             throw new RuntimeException(e);
         } finally {
-            // 打印出参
-            log.info("Response Args  : {}", JSONUtil.toJsonPrettyStr(result));
-            // 执行耗时
-            log.info("Time-Consuming : {} ms", System.currentTimeMillis() - startTime);
+            // 开始打印请求日志
+            ServletRequestAttributes attributes =
+                    (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+            log.info("""
+                                ========================================== Start ==========================================
+                                URL            : {},
+                                HTTP Method    : {},
+                                Class Method   : {}.{},
+                                IP             : {},
+                                Request Args   : {},
+                                Response Args  : {},
+                                Time-Consuming : {} ms
+                            """, request.getRequestURL().toString(), request.getMethod(),
+                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(),
+                    request.getRemoteAddr(), JSONUtil.toJsonPrettyStr(joinPoint.getArgs()),
+                    JSONUtil.toJsonPrettyStr(result), System.currentTimeMillis() - startTime);
         }
         return result;
     }
